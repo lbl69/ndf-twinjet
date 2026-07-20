1efc1acd23aefbb36f35e2da8dd06a292bd415d0
lbl69 <loris.bc@outlook.fr>
2026-07-21 00:26:40 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* NDF Twin Jet — service worker (app shell, cache-first) */
-const CACHE = "ndf-v19";
+const CACHE = "ndf-v20";
 const ASSETS = ["./", "./index.html", "./manifest.webmanifest", "./icon-192.png", "./icon-512.png", "./icon-512-maskable.png", "./apple-touch-icon.png", "./jspdf.umd.min.js", "./html2canvas.min.js"];
 
 self.addEventListener("install", (e) => {
